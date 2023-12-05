ebd79009a07bc20c809daa6eba084a7e568ff026
Franklin Dantas <57151869+Franklin-Dantas@users.noreply.github.com>
2023-12-05 16:51:49 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/dozag/MainActivity.kt
+++ app/src/main/java/com/example/dozag/MainActivity.kt
@@ -1,39 +1,78 @@
-package com.example.dozag
+package com.example.dozag.list_item
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.dozag.ui.theme.DozagTheme
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.dozag.R
+import com.example.dozag.model.Sticker
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            DozagTheme {
-                LojaSticker()
-            }
+@Composable
+fun StickerItem(
+
+){
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Black)
+            .padding(0.dp, 10.dp, 0.dp, 10.dp)
+    ) {
+
+        val(
+            imgSticker,
+            txtStickerName,
+            txtStickerDescription,
+            txtPrice,
+            btAddCart,
+            conteinerImg,
+            rowItem
+        ) = createRefs()
+        Card (
+            modifier = Modifier
+                .constrainAs(conteinerImg) {
+                    top.linkTo(parent.top,0.dp)
+                    start.linkTo(parent.start,0.dp)
+
+                }
+                .size(130.dp)
+                .padding(20.dp),
+            //passar cor de fundo
+            colors = CardDefaults.cardColors(
+            ),
+            shape = ShapeDefaults.Medium,
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 8.dp
+            )
+        ){
+
         }
-    }
 
-    @Composable
-    private fun LojaSticker(){
-        Text(text = "Loja de Stickers")
-        Column (modifier = Modifier.fillMaxSize().background(color = Color.Black)){
-
-        }
+        Image(
+            painter = painterResource(id = R.drawable.sticker1) ,
+            contentDescription = null,
+            modifier = Modifier
+                //finalizar
+                .constrainAs(imgSticker) {
+                    
+                }
+                .size(60.dp)
+        )
     }
-
-    @Preview
-    @Composable
-    fun PrimeiraTelaPreview() {
-        LojaSticker()
-    }
+}
+@Composable
+@Preview
+private fun StickerItemPreview(){
+    StickerItem()
 }
