ed1177f4a61da1b3f8bdcaf38b3088178fc6b38c
Franklin Dantas <57151869+Franklin-Dantas@users.noreply.github.com>
2023-12-07 16:34:44 -0300

    Finalizando  StickerItem

--- app/src/main/java/com/example/dozag/list_item/StickerItem.kt
+++ app/src/main/java/com/example/dozag/list_item/StickerItem.kt
@@ -1,9 +1,14 @@
 package com.example.dozag.list_item
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ShapeDefaults
@@ -47,7 +52,6 @@ fun StickerItem(
                 .constrainAs(conteinerImg) {
                     top.linkTo(parent.top, 0.dp)
                     start.linkTo(parent.start, 0.dp)
-
                 }
                 .size(130.dp)
                 .padding(20.dp),
@@ -59,7 +63,6 @@ fun StickerItem(
                 defaultElevation = 8.dp
             )
         ) {
-
         }
 
         Image1(
@@ -79,7 +82,7 @@ fun StickerItem(
         Text(
             text = "Stiker Pato",
             fontSize = 18.sp,
-            fontStyle = FontWeight.Bold,
+            fontWeight = FontWeight.Bold,
             color = Color.Red,
             modifier = Modifier.constrainAs(txtStickerName){
                 top.linkTo(parent.top,30.dp)
@@ -91,13 +94,15 @@ fun StickerItem(
         Text(
             text = "Essa figurinha pertence ao primeiro pack",
             fontSize = 14.sp,
-            fontStyle = FontWeight.Bold,
+            fontWeight = FontWeight.Bold,
             color = Color.White,
-            modifier = Modifier.constrainAs(txtStickerDescription){
-                top.linkTo(txtStickerName.bottom)
-                start.linkTo(conteinerImg.end,0.dp)
-                end.linkTo(parent.end)
-            }.padding(65.dp,10.dp,40.dp,0.dp),
+            modifier = Modifier
+                .constrainAs(txtStickerDescription) {
+                    top.linkTo(txtStickerName.bottom)
+                    start.linkTo(conteinerImg.end, 0.dp)
+                    end.linkTo(parent.end)
+                }
+                .padding(65.dp, 10.dp, 40.dp, 0.dp),
             maxLines = 2
         )
 
@@ -105,7 +110,7 @@ fun StickerItem(
         Text (
             text = "5 R$",
             fontSize = 18.sp,
-            fontStyle = FontWeight.Bold,
+            fontWeight = FontWeight.Bold,
             color = Color.White,
             modifier = Modifier.constrainAs(txtPrice){
                 top.linkTo(txtStickerDescription.bottom,20.dp)
@@ -113,9 +118,42 @@ fun StickerItem(
             }
         )
 
+        Button(
+            onClick = {
+
+        },
+            modifier = Modifier
+                .height(45.dp)
+                .constrainAs(btAddCart) {
+                    top.linkTo(txtStickerDescription.bottom, 10.dp)
+                    start.linkTo(txtPrice.end, 10.dp)
+                    end.linkTo(parent.end, 10.dp)
+                },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF44335))
+            ){
+            Text(
+                text = "Add Cart",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
+
+        Row (
+            modifier = Modifier
+                .width(420.dp)
+                .height(4.dp)
+                .background(color = Color.Red)
+                .constrainAs(rowItem){
+                    top.linkTo(txtPrice.bottom,30.dp)
+                    start.linkTo(parent.start,20.dp)
+                    end.linkTo(parent.end,20.dp)
+                }
+        ){
+        }
     }
 }
-
 
 @Composable
 @Preview
